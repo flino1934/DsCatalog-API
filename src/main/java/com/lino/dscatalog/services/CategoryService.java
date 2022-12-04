eb062e702ace12965fb3600b6a5f7c279a6aeeac
flino1934 <f.lino1934@hotmail.com>
2022-12-04 17:10:27 -0300

    Criando o metodo update
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/dscatalog/services/CategoryService.java
+++ src/main/java/com/lino/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,6 +51,21 @@ public class CategoryService {
 		entity = categoryRepository.save(entity);
 
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		// TODO Auto-generated method stub
+		try {
+
+			Category entity = categoryRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity = categoryRepository.save(entity);
+
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id não encontrado " + id);
+		}
 	}
 
 }
