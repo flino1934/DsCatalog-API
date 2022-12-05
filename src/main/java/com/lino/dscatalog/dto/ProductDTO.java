c69a65c470d91176d66be77148121f405be319f2
flino1934 <f.lino1934@hotmail.com>
2022-12-05 10:51:44 -0300

    Implementando categoryDTO
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

---
+++ src/main/java/com/lino/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,112 @@
+package com.lino.dscatalog.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.lino.dscatalog.entities.Category;
+import com.lino.dscatalog.entities.Product;
+
+public class ProductDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	private Instant date;
+
+	private List<CategoryDTO> categories = new ArrayList<>();
+
+	public ProductDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
+	
+	public ProductDTO(Product entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.description = entity.getDescription();
+		this.price = entity.getPrice();
+		this.imgUrl = entity.getImgUrl();
+		this.date = entity.getDate();
+	}
+	
+	//Sobre carga  vai carregar o dto colocando  os elementos na lista de categories mas só quando chamar ese contrutou
+	public ProductDTO (Product entity,Set<Category> categories) {
+		//vai chamar o construtor de (public ProductDTO(Product entity){)e executa o construtor
+		this(entity);
+		//vai percorrer o conhunto de categorias e inserir como um novo categoryDTO na lista la de cima de tudo
+		//E ja transforma para DTO
+		categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public List<CategoryDTO> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<CategoryDTO> categories) {
+		this.categories = categories;
+	}
+
+}
