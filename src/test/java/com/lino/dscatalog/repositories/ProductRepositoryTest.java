ab1d37dd2067a2049e259391e7bed9ed26a2032a
flino1934 <f.lino1934@hotmail.com>
2022-12-11 20:22:07 -0300

    Implementando o beforeach
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/repositories/ProductRepositoryTest.java
+++ src/test/java/com/lino/dscatalog/repositories/ProductRepositoryTest.java
@@ -3,6 +3,7 @@ package com.lino.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,10 +17,18 @@ public class ProductRepositoryTest {
 	@Autowired
 	private ProductRepository productRepository;
 
+	private long existingById;
+	private long nonExistingById;
+	
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingById = 1L;
+		nonExistingById = 123;
+	}
+	
 	@Test
 	public void deleteShouldObjectWhenIdExist() {
-
-		long existingById = 1L;
 
 		productRepository.deleteById(existingById);
 
@@ -31,12 +40,10 @@ public class ProductRepositoryTest {
 	@Test
 	public void deleteShouldThrowEmptyResutDataAcessExceptionWhenIdDoesNotExist() {
 
-		long existingById = 132L;
-
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-			productRepository.deleteById(existingById);
+			productRepository.deleteById(nonExistingById);
 		});
 
 	}
-
+	
 }
