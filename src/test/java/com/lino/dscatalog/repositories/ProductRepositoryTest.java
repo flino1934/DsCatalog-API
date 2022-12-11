b9153a50a3441b00012f6ede3ff4ec3053b7e4aa
flino1934 <f.lino1934@hotmail.com>
2022-12-11 20:05:31 -0300

    Criando teste do delete
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/test/java/com/lino/dscatalog/repositories/ProductRepositoryTest.java
+++ src/test/java/com/lino/dscatalog/repositories/ProductRepositoryTest.java
@@ -6,25 +6,37 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.lino.dscatalog.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTest {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Test
 	public void deleteShouldObjectWhenIdExist() {
-		
+
 		long existingById = 1L;
-		
+
 		productRepository.deleteById(existingById);
-		
+
 		Optional<Product> result = productRepository.findById(existingById);
 		Assertions.assertFalse(result.isPresent());
-		
+
+	}
+
+	@Test
+	public void deleteShouldThrowEmptyResutDataAcessExceptionWhenIdDoesNotExist() {
+
+		long existingById = 132L;
+
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			productRepository.deleteById(existingById);
+		});
+
 	}
 
 }
